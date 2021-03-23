0f39d62aa495a897fe72b308a8271335f6a564cf
vankicha <ivanip.burgas@gmail.com>
2021-03-24 01:19:00 +0200

    Style header

--- client/src/components/layouts/Main/Header/Header.js
+++ client/src/components/layouts/Main/Header/Header.js
@@ -1,14 +1,24 @@
 import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
 import './Header.scss';
 
 const Header = () => {
     return (
-        <header className='header-wrapper'>
-            <span>REACT A CAR</span>
-            <ul>
-                <li>Login</li>
-                <li>Register</li>
-            </ul>
+        <header>
+            <Navbar>
+                <Navbar.Brand>
+                    <Link to='/'>REACT A CAR</Link>
+                </Navbar.Brand>
+                <ul className='logged-links'>
+                    <Link to='/profile'>My Profile</Link>
+                    <Link to='/about'>About Us</Link>
+                </ul>
+                {/* <ul className='guest-links'>
+                    <Link to='/login'>Login</Link>
+                    <Link to='/register'>Register</Link>
+                </ul> */}
+                <button>LOGOUT</button>
+            </Navbar>
         </header>
     );
 };
