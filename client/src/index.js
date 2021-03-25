d9caf9e232b25055704683f126d38320c0c0d942
vankicha <ivanip.burgas@gmail.com>
2021-03-26 01:48:09 +0200

    Initialize redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import store from './utils/store';
+import { Provider } from 'react-redux';
 import './index.scss';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>,
     document.getElementById('root')
 );
