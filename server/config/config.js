5444d98f796fdb10f37dda15ccfec63ba9b49f92
vankicha <ivanip.burgas@gmail.com>
2021-04-06 13:16:21 +0300

    Update config

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,16 @@
 const config = {
     development: {
         PORT: 5000,
-        DB_CONNECTION: 'mongodb+srv://admin:admin@cluster0.hxzyr.mongodb.net/react-a-car',
+        DB_CONNECTION:
+            'mongodb+srv://admin:admin@cluster0.hxzyr.mongodb.net/react-a-car',
         SALT_ROUNDS: 1,
     },
     production: {
         PORT: 80,
-        DB_CONNECTION: 'mongodb+srv://admin:admin@cluster0.hxzyr.mongodb.net/react-a-car',
+        DB_CONNECTION:
+            'mongodb+srv://admin:admin@cluster0.hxzyr.mongodb.net/react-a-car',
         SALT_ROUNDS: 5,
     },
 };
 
-module.exports = config[process.env.NODE_ENV.trim()];
+module.exports = config[process.env.NODE_ENV];
